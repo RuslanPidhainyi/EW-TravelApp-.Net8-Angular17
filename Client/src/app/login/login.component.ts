43aed0ca41c5fabab8db92938f824babad7a58d2
Ruslan <ruslanpidhainyi02@gmail.com>
2024-10-10 14:44:00 +0200

    Added a toast service for notifications

--- Client/src/app/login/login.component.ts
+++ Client/src/app/login/login.component.ts
@@ -1,20 +1,24 @@
 import { Component, inject, OnInit } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { AccountService } from '../_services/account.service';
-import { RegisterComponent } from "../register/register.component";
+import { RegisterComponent } from '../register/register.component';
 import { HttpClient } from '@angular/common/http';
+import { ToastrService } from 'ngx-toastr';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   standalone: true,
   imports: [FormsModule, RegisterComponent],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.scss'
+  styleUrl: './login.component.scss',
 })
 export class LoginComponent implements OnInit {
   http = inject(HttpClient);
   accountService = inject(AccountService);
-  registerMode = false; 
+  private router = inject(Router);
+  private toastr = inject(ToastrService);
+  registerMode = false;
   model: any = {};
   users: any;
 
@@ -24,27 +28,28 @@ export class LoginComponent implements OnInit {
 
   login() {
     this.accountService.login(this.model).subscribe({
-      next: response => {
-        console.log(response);
+      next: () => {
+        this.router.navigateByUrl('/offers');
       },
-      error: error => console.log(error),
-    })
+      error: (error) => {
+        this.toastr.error(error.error);
+      },
+    });
   }
-  
+
   registerToggle() {
-    this.registerMode = !this.registerMode
+    this.registerMode = !this.registerMode;
   }
 
   cancelRegisterMode(event: boolean) {
-    this.registerMode = event;  
+    this.registerMode = event;
   }
 
   getUsers() {
     this.http.get('https://localhost:5001/api/users').subscribe({
-      next: response => this.users = response,
-      error: error => console.error(error),
+      next: (response) => (this.users = response),
+      error: (error) => console.error(error),
       complete: () => console.log('Request has completed'),
     });
   }
-  
 }
--- Client/src/app/nav/nav.component.ts
+++ Client/src/app/nav/nav.component.ts
@@ -2,7 +2,8 @@ import { Component, inject } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { AccountService } from '../_services/account.service';
 import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
-import { RouterLink } from '@angular/router';
+import { Router, RouterLink } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-nav',
@@ -13,18 +14,23 @@ import { RouterLink } from '@angular/router';
 })
 export class NavComponent {
   accountService = inject(AccountService);
+  private router = inject(Router);
+  private toastr = inject(ToastrService);
   model: any = {};
 
   login() {
     this.accountService.login(this.model).subscribe({
-      next: resp => {
-        console.log(resp);
+      next: () => {
+        this.router.navigateByUrl('/offers');
       },
-      error: err => console.log(err),
-    })
+      error: error => {
+        this.toastr.error(error.error);
+      },
+    });
   }
 
   logout() {
     this.accountService.logout();
+    this.router.navigateByUrl('/');
   }
 }
