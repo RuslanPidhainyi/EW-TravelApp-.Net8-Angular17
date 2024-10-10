91338b528c2f87b748e4a6ccdaf2b6b3716df82e
Ruslan <ruslanpidhainyi02@gmail.com>
2024-10-10 13:32:35 +0200

    Created some more components - v 0.4.0

--- Client/src/app/app.routes.ts
+++ Client/src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { OffersListComponent } from './offers/offers-list/offers-list.component';
+import { OffersDetailComponent } from './offers/offers-detail/offers-detail.component';
+import { ListsComponent } from './lists/lists.component';
+import { MessagesComponent } from './messages/messages.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: LoginComponent },
+  { path: 'offers', component: OffersListComponent },
+  { path: 'offers/:id', component: OffersDetailComponent },
+  { path: 'lists', component: ListsComponent },
+  { path: 'messages', component: MessagesComponent },
+  { path: '**', component: LoginComponent, pathMatch: 'full' },
+];
---
+++ Client/src/app/offers/offers-detail/offers-detail.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-offers-detail',
+  standalone: true,
+  imports: [],
+  templateUrl: './offers-detail.component.html',
+  styleUrl: './offers-detail.component.scss'
+})
+export class OffersDetailComponent {
+
+}
