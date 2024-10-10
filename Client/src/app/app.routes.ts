5d4b3ea24c67d02f9d2186dc0c915cfb73e47a7f
Ruslan <ruslanpidhainyi02@gmail.com>
2024-10-10 15:08:17 +0200

    Added an Angular route guard - v 0.4.2

--- Client/src/app/app.routes.ts
+++ Client/src/app/app.routes.ts
@@ -4,12 +4,22 @@ import { OffersListComponent } from './offers/offers-list/offers-list.component'
 import { OffersDetailComponent } from './offers/offers-detail/offers-detail.component';
 import { ListsComponent } from './lists/lists.component';
 import { MessagesComponent } from './messages/messages.component';
+import { authGuard } from './_guards/auth.guard';
 
 export const routes: Routes = [
-  { path: '', component: LoginComponent },
-  { path: 'offers', component: OffersListComponent },
-  { path: 'offers/:id', component: OffersDetailComponent },
-  { path: 'lists', component: ListsComponent },
-  { path: 'messages', component: MessagesComponent },
-  { path: '**', component: LoginComponent, pathMatch: 'full' },
+    { path: '', component: LoginComponent },
+    {
+        path: '',
+        runGuardsAndResolvers: 'always',
+        canActivate: [authGuard],
+        children: [
+            { path: '', component: LoginComponent },
+            { path: 'offers', component: OffersListComponent },
+            { path: 'offers/:id', component: OffersDetailComponent },
+            { path: 'lists', component: ListsComponent },
+            { path: 'messages', component: MessagesComponent },
+            { path: '**', component: LoginComponent, pathMatch: 'full' },
+        ],
+    },
+    { path: '**', component: LoginComponent, pathMatch: 'full', },
 ];
--- Client/src/app/nav/nav.component.ts
+++ Client/src/app/nav/nav.component.ts
@@ -4,11 +4,12 @@ import { AccountService } from '../_services/account.service';
 import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
 import { Router, RouterLink } from '@angular/router';
 import { ToastrService } from 'ngx-toastr';
+import { TitleCasePipe } from '@angular/common';
 
 @Component({
   selector: 'app-nav',
   standalone: true,
-  imports: [FormsModule, BsDropdownModule, RouterLink],
+  imports: [FormsModule, BsDropdownModule, RouterLink, TitleCasePipe],
   templateUrl: './nav.component.html',
   styleUrl: './nav.component.scss',
 })
