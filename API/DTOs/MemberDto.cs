9ff736acfd81479e2c853906dbac5c5861331a82
Ruslan <ruslanpidhainyi02@gmail.com>
2024-10-14 18:19:08 +0200

    Added a DTO for Members - v 0.6.6

---
+++ API/DTOs/MemberDto.cs
@@ -0,0 +1,21 @@
+using System;
+
+namespace API.DTOs;
+
+public class MemberDto
+{
+    public int Id { get; set; }
+    public string? UserName { get; set; }
+    public int Age { get; set; }
+    public string? GeneralPhotoUrl { get; set; }
+    public string? KnownAs { get; set; }
+    public DateTime Created { get; set; }
+    public DateTime LastActive { get; set; }
+    public string? Gender { get; set; }
+    public string? Description { get; set; }
+    public string? Interests { get; set; }
+    public string? Country { get; set; }
+    public string? City { get; set; }
+    public List<PhotoDto>? GeneralPhotos { get; set; }
+    //public List<PhotoPostsDto>? PhotosOfPosts {get; set;}
+}
