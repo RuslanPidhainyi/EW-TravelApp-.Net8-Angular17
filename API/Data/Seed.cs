4757e0769dd477f5266a1dc4dc7a08763c1dedf8
Ruslan <ruslanpidhainyi02@gmail.com>
2024-10-14 16:04:26 +0200

    Seeded data part two - v 0.6.4

--- API/Data/Seed.cs
+++ API/Data/Seed.cs
@@ -11,12 +11,11 @@ public class Seed
 {
     public static async Task SeedUsers(AppDbContext context)
     {
+
         if (await context.Users.AnyAsync()) return;
 
         var userData = await File.ReadAllTextAsync("Data/UserSeedData.json");
-
         var options = new JsonSerializerOptions { PropertyNameCaseInsensitive = true };
-
         var users = JsonSerializer.Deserialize<List<AppUser>>(userData, options);
 
         if (users == null) return;
@@ -30,6 +29,7 @@ public class Seed
             user.PasswordSalt = hmac.Key;
 
             context.Users.Add(user);
+
         }
 
         await context.SaveChangesAsync();
