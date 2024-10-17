0d54401d3c372170f47feae14e7ccbd2fcba2dc3
Ruslan <ruslanpidhainyi02@gmail.com>
2024-10-17 22:06:59 +0200

    Routed to the detailed page - v 0.8.7

--- Client/src/app/offers/offer-card/offer-card.component.ts
+++ Client/src/app/offers/offer-card/offer-card.component.ts
@@ -1,11 +1,12 @@
 import { Component, input } from '@angular/core';
 import { Post } from '../../_models/post';
 import { TitleCasePipe } from '@angular/common';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-offer-card',
   standalone: true,
-  imports: [TitleCasePipe],
+  imports: [TitleCasePipe, RouterLink],
   templateUrl: './offer-card.component.html',
   styleUrl: './offer-card.component.scss'
 })
--- Client/src/app/offers/offers-detail/offers-detail.component.ts
+++ Client/src/app/offers/offers-detail/offers-detail.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { PostsService } from '../../_services/posts.service';
+import { ActivatedRoute } from '@angular/router';
+import { Post } from '../../_models/post';
 
 @Component({
   selector: 'app-offers-detail',
@@ -7,6 +10,20 @@ import { Component } from '@angular/core';
   templateUrl: './offers-detail.component.html',
   styleUrl: './offers-detail.component.scss'
 })
-export class OffersDetailComponent {
+export class OffersDetailComponent implements OnInit {
+  private postService = inject(PostsService);
+  private route = inject(ActivatedRoute);
+  post?: Post;
 
+  ngOnInit(): void {
+    this.loadPost()
+  }
+
+  loadPost() {
+    const id = this.route.snapshot.paramMap.get('id');
+    if (!id) return;
+    this.postService.getPost(id).subscribe({
+      next: post => this.post = post,
+    });
+  } 
 }
