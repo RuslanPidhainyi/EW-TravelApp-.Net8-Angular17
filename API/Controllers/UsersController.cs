530d89fd524d0612baa960c4fc4618cd56f5a26a
Ruslan <ruslanpidhainyi02@gmail.com>
2024-10-22 15:43:43 +0200

    Persisted the changes in the API - v 0.9.4

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Security.Claims;
 using API.Controllers;
 using API.Data;
 using API.DTOs;
@@ -12,7 +13,7 @@ using Microsoft.EntityFrameworkCore;
 namespace API.Controllers;
 
 [Authorize]
-public class UsersController(IUserRepository userRepo) : BaseApiController
+public class UsersController(IUserRepository userRepo, IMapper mapper) : BaseApiController
 {
     [HttpGet] // /api/users
     public async Task<ActionResult<IEnumerable<MemberDto>>> GetUsers()
@@ -30,5 +31,23 @@ public class UsersController(IUserRepository userRepo) : BaseApiController
         if (user == null) return NotFound();
 
         return user;
+    }
+
+    [HttpPut]
+    public async Task<ActionResult> UpdateUser(MemberUpdatedDto memberUpdateDto)
+    {
+        var username = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+
+        if (username == null) return BadRequest("No username found in token");
+
+        var user = await userRepo.GetUserByUsernameAsync(username);
+
+        if (user is null) return BadRequest("Could not found user");
+        
+        mapper.Map(memberUpdateDto, user);
+
+        if (await userRepo.SaveAllAsync()) return NoContent();
+
+        return BadRequest("Failed to update the user");
     }
 }
