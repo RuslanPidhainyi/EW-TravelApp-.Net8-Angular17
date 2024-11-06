8f117447c4f0e5ac8c7286d734ce6e693145e364
Ruslan <ruslanpidhainyi02@gmail.com>
2024-11-06 10:10:53 +0100

    Fixed mistake - v 0.11.13.

--- Client/src/app/_services/posts.service.ts
+++ Client/src/app/_services/posts.service.ts
@@ -18,13 +18,6 @@ export class PostsService {
     });
   }
 
-  // getPostById(id: number) {
-  //   const post = this.posts().find((x) => x.id === id);
-  //   if (post !== undefined) return of(post);
-
-  //   return this.http.get<Post>(this.baseUrl + 'posts/' + id);
-  // }
-
   getPostById(id: number): Observable<Post> {
     return this.http.get<Post>(`${this.baseUrl}posts/${id}`).pipe(
       tap((post) => {
@@ -35,15 +28,6 @@ export class PostsService {
     );
   }
 
-  /* olde version method - getPostByUsername
-  getPostByUsername(username: string) {
-    const post = this.posts().find((x) => x.userName === username);
-    if (post !== undefined) return of(post);
-
-    return this.http.get<Post[]>(`${this.baseUrl}posts/user/${username}`);
-  }
-*/
-
   getPostByUsername(username: string): Observable<Post[]> {
     const posts = this.posts().filter((x) => x.userName === username);
     if (posts.length > 0) return of(posts);
@@ -52,64 +36,15 @@ export class PostsService {
   }
 
   addPost(postData: FormData): Observable<any> {
-    return this.http.post<any>(`${this.baseUrl}posts/add-post`, postData);
+    return this.http.post<any>(`${this.baseUrl}posts/add-post`, postData).pipe();
   }
-
-  // addPost(postData: FormData): Observable<Post> {
-  //   return this.http.post<Post>(`${this.baseUrl}posts/add-post`, postData).pipe(
-  //     tap((newPost) => {
-  //       // Додаємо новий пост на початок списку `posts`
-  //       this.posts.update((posts) => [newPost, ...posts]);
-  //     })
-  //   );
-  // }
-
-  // addPost(postData: FormData): Observable<any> {
-  //   return this.http.post<any>(`${this.baseUrl}posts/add-post`, postData).pipe(
-  //     // tap(() => {
-  //     //   // Оновити список постів, якщо потрібно
-  //     // })
-  //     tap((newPost) => {
-  //       // Додаємо новий пост до сигналу `posts`
-  //       this.posts.update((posts) => [newPost, ...posts]);
-  //     })
-  //   );
-  // }
-
-  ///////////////////
-  // updatePost(id: number, postData: FormData): Observable<any> {
-  //   return this.http.put<any>(`${this.baseUrl}posts/edit-post/${id}`, postData);
-  // }
-
-  // // Метод для оновлення кешованого поста в сигналі `posts`
-  // updateCachedPost(updatedPost: Post) {
-  //   const updatedPosts = this.posts().map((post) =>
-  //     post.id === updatedPost.id ? updatedPost : post
-  //   );
-  //   this.posts.set(updatedPosts);
-  // }
-
-  //////////////////////
-
-  // updatePost(id: number, post: Post) {
-  //   return this.http
-  //     .put(`${this.baseUrl}posts/edit-post/${post.id}`, post)
-  //     .pipe(
-  //       tap(() => {
-  //         // Оновити список постів, якщо потрібно
-  //       })
-  //     );
-  // }
 
   updatePost(id: number, postData: FormData) {
     return this.http.put(`${this.baseUrl}posts/edit-post/${id}`, postData).pipe(
-      tap(() => {
-        // Оновити список постів, якщо потрібно
-      })
     );
   }
 
-  deletePost(id: number): Observable<any> {
-    return this.http.delete<any>(`${this.baseUrl}posts/delete-post/${id}`);
+  deletePost(id: number) {
+    return this.http.delete(`${this.baseUrl}posts/delete-post/${id}`).pipe();
   }
 }
