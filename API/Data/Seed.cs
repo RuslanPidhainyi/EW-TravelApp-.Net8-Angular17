42d74b6c44f3bffb6b662a8a4bf33d4cb6bfe435
Ruslan <ruslanpidhainyi02@gmail.com>
2024-11-21 06:36:58 +0100

    Setted up the entities - v 0.16.0

--- API/Data/Seed.cs
+++ API/Data/Seed.cs
@@ -25,11 +25,8 @@ public class Seed
             using var hmac = new HMACSHA512();
 
             user.UserName = user.UserName.ToLower();
-            user.PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes("Pa$$w0rd2024"));
-            user.PasswordSalt = hmac.Key;
 
             context.Users.Add(user);
-
         }
 
         await context.SaveChangesAsync();
