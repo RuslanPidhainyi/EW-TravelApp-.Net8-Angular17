71f285cc9b7bbeab97a3f1d6da26f71c96dd5109
Ruslan <ruslanpidhainyi02@gmail.com>
2024-11-21 16:54:39 +0100

    Updated the account controller - v 0.16.2

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -5,45 +5,48 @@ using API.DTOs;
 using API.Entities;
 using API.Interface;
 using AutoMapper;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers;
 
-public class AccountController(AppDbContext context, ITokenService tokenService, IMapper mapper) : BaseApiController
+public class AccountController(UserManager<AppUser> userManager, ITokenService tokenService, IMapper mapper) : BaseApiController
 {
     [HttpPost("register")] //account/register
     public async Task<ActionResult<UserDto>> Register(RegisterDto registerDto)
     {
         if (await UserExists(registerDto.Username)) return BadRequest("Username is taken");
 
-        using var hmac = new HMACSHA512();
-
         var user = mapper.Map<AppUser>(registerDto);
 
         user.UserName = registerDto.Username.ToLower();
 
-        context.Users.Add(user);
-        await context.SaveChangesAsync();
+        var result = await userManager.CreateAsync(user, registerDto.Password);
+
+        if(!result.Succeeded) return BadRequest(result.Errors);
 
         return new UserDto
         {
             Username = user.UserName,
             Token = tokenService.CreateToken(user),
             KnownAs = user.KnownAs,
-            //Gender = user.Gender
         };
     }
 
     [HttpPost("login")]
     public async Task<ActionResult<UserDto>> Login(LoginDto loginDto)
     {
-        var user = await context.Users
+        var user = await userManager.Users
             .Include(p => p.GeneralPhotos)
             .FirstOrDefaultAsync(x =>
-            x.UserName == loginDto.Username.ToLower());
+            x.NormalizedUserName == loginDto.Username.ToUpper());
 
         if (user == null || user.UserName == null) return Unauthorized("Invalid username");
+
+        var userPasssword = await userManager.CheckPasswordAsync(user, loginDto.Password);
+
+        if(!userPasssword) return Unauthorized("Invalid password");
 
         return new UserDto
         {
@@ -51,12 +54,11 @@ public class AccountController(AppDbContext context, ITokenService tokenService,
             Token = tokenService.CreateToken(user),
             GeneralPhotoUrl = user.GeneralPhotos.FirstOrDefault(x => x.IsMain)?.Url,
             KnownAs = user.KnownAs,
-            //Gender = user.Gender
         };
     }
 
     private async Task<bool> UserExists(string username)
     {
-        return await context.Users.AnyAsync(x => x.NormalizedUserName == username.ToUpper()); 
+        return await userManager.Users.AnyAsync(x => x.NormalizedUserName == username.ToUpper()); 
     }
 }
