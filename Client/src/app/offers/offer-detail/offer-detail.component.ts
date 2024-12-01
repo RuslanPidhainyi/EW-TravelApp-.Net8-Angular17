5c3222e0147a0f8431b8d225eb469db26fae14ee
Ruslan <ruslanpidhainyi02@gmail.com>
2024-12-01 23:10:17 +0100

    UPDATED: Unimplemented version modal window for detail post - v 0.18.13

--- Client/src/app/offers/offer-detail/offer-detail.component.ts
+++ Client/src/app/offers/offer-detail/offer-detail.component.ts
@@ -61,3 +61,61 @@ export class OfferDetailComponent implements OnInit {
     });
   }
 }
+
+// Modal window version  
+
+// import { Component, inject, Input, OnInit } from '@angular/core';
+// import { BsModalRef } from 'ngx-bootstrap/modal';
+// import { PostsService } from '../../_services/posts.service';
+// import { Post } from '../../_models/post';
+// import { LikesService } from '../../_services/likes.service';
+// import { PresenceService } from '../../_services/presence.service';
+// import { TitleCasePipe } from '@angular/common';
+// import { RouterLink } from '@angular/router';
+
+// @Component({
+//   selector: 'app-offer-detail',
+//   standalone: true,
+//   imports: [TitleCasePipe],
+//   templateUrl: './offer-detail.component.html',
+//   styleUrl: './offer-detail.component.scss',
+// })
+// export class OfferDetailComponent implements OnInit {
+//   bsModalRef = inject(BsModalRef);
+//   private postService = inject(PostsService);
+//   private likeService = inject(LikesService);
+//   private presenceService = inject(PresenceService);
+
+//   @Input() postId!: number; // Отримує ID поста через модальне вікно
+//   post: Post | null = null;
+
+//   hasLiked = () => this.post && this.likeService.likeIds().includes(this.post.id);
+//   isOnline = () => this.post && this.presenceService.onlineUsers().includes(this.post.userName);
+
+//   ngOnInit(): void {
+//     this.loadPost();
+//   }
+
+//   loadPost() {
+//     this.postService.getPostById(this.postId).subscribe({
+//       next: (post) => (this.post = post),
+//       error: (err) => console.error('Failed to load post:', err),
+//     });
+//   }
+
+//   toggleLike() {
+//     if (!this.post) return;
+
+//     this.likeService.toggleLike(this.post.id).subscribe(() => {
+//       if (this.hasLiked()) {
+//         this.likeService.likeIds.update((ids) => ids.filter((x) => x !== this.post!.id));
+//       } else {
+//         this.likeService.likeIds.update((ids) => [...ids, this.post!.id]);
+//       }
+//     });
+//   }
+
+//   closeModal() {
+//     this.bsModalRef.hide();
+//   }
+// }
